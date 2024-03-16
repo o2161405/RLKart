26f23395bbb67b32f42af5a5539e9a87587982a0
o2161405 <91959604+o2161405@users.noreply.github.com>
2024-03-16 09:51:39 +0000

    Code comments

--- source/main.py
+++ source/main.py
@@ -8,6 +8,7 @@ from pygame_ui import draw_grid_with_intersect_locations
 import numpy as np
 import math
 
+# TODO: only calculate Z and return that, faster.
 def euler_from_quaternion(x, y, z, w):
         """
         Convert a quaternion into euler angles (roll, pitch, yaw)
@@ -30,6 +31,7 @@ def euler_from_quaternion(x, y, z, w):
      
         return roll_x * 57.295779513, pitch_y * 57.295779513, yaw_z * 57.295779513 # in radians
 
+# Hook into Dolphin
 while not DME.is_hooked():
     print("Not hooked..")
     DME.hook()
@@ -39,42 +41,45 @@ while True:
 
 	t0 = time.time()
 
+	# Get game state
 	game_state = get_game_state()
 
+	# In menus check
 	if game_state == None:
 		continue
 
-	definition = 55
-	inbetween_gap = 2
+	# 𝑥 by 𝑥 grid output from raycasting
+	definition = 75
+	# Gap inbetween each ray, measured in meters. Higher value = Less zoom (and less quality)
+	inbetween_gap = 1.5
 
+	# Raycast, and then remove unnecessary X and Z component from ray output
+	# (Since rays are always looking down, we don't need X and Z)
 	intersect_locations = calculate_intersect_locations(positions=game_state["Current_Positions"], inbetween_gap=inbetween_gap, definition=definition)
 	intersect_filtered = [round(i[1]) for i in intersect_locations]
 	intersect_filtered = np.array(intersect_filtered).reshape(definition, definition)
 
-	""" Code for adding new things to the overlay:
+	""" # Code for adding new things to the overlay:
 	numbers = {0}
 	for i in intersect_locations:
 		numbers.add(round(i[1], 2))
 	print(numbers)
 	exit()"""
 
-
-	#print(intersect_locations)
-	#print(len(intersect_locations))
-
-	# Pygame Overlay
-	#draw_grid_with_intersect_locations(intersect_locations=intersect_locations, grid_size=definition, cell_size=10)
-
-
+	# Get rotation, since raycasts are always pointing the same direction, the AI agent needs to know where it's looking towards
 	x, y, z, w = game_state["Quaternions"]
 	# MKW swaps y and z idk
 	rotation = round(euler_from_quaternion(x, z, y, w)[2])
 
+	# Pygame Overlay
+	draw_grid_with_intersect_locations(intersect_locations=intersect_locations, grid_size=definition, cell_size=10, rotation=rotation)
+
 	print((time.time() - t0)*1000)
 
 	sleep(0.1)
-	#os.system('cls')
 
+
+# IGNORE THIS FOR NOW
 # We're doing 0:3 for the agent's view because index 4 and 5 are the quaternion and position info.
 # Those two things aren't useful for the AI agent to have, but we still need
 # them in the output for when we do the raycasting stuff.
--- source/pygame_ui.py
+++ source/pygame_ui.py
@@ -1,6 +1,8 @@
 import pygame
+import math
 
 pygame.init()
+LINE_LENGTH = 100
 
 #OFFROAD = (145, 103, 65)
 #ROAD = (180, 180, 180)
@@ -9,14 +11,14 @@ pygame.init()
 #BOOST = (255, 255, 0)
 #UNKNOWN = (255, 0, 0)
 
-def draw_grid_with_intersect_locations(intersect_locations, grid_size, cell_size):
+def draw_grid_with_intersect_locations(intersect_locations, grid_size, cell_size, rotation):
 
     GRID_SIZE = grid_size
     CELL_SIZE = cell_size
     WINDOW_SIZE = (GRID_SIZE * CELL_SIZE, GRID_SIZE * CELL_SIZE)
-
-    screen = pygame.display.set_mode(WINDOW_SIZE)
-
+    
+    screen = pygame.display.set_mode(WINDOW_SIZE, vsync=1)
+    
     def draw_grid(grid_1d):
         for row in range(GRID_SIZE):
             for col in range(GRID_SIZE):
@@ -62,6 +64,15 @@ def draw_grid_with_intersect_locations(intersect_locations, grid_size, cell_size
 
     # Draw the grid
     draw_grid(intersect_locations)
+
+    LINE_START = (WINDOW_SIZE[0]/2,  WINDOW_SIZE[1]/2)
+
+    pygame.draw.circle(screen, (150, 255, 255), LINE_START, radius=10)
+
+    angle_rad = math.radians(rotation)
+    line_end_x = LINE_START[0] + LINE_LENGTH * math.cos(-angle_rad)
+    line_end_y = LINE_START[1] - LINE_LENGTH * math.sin(-angle_rad)
+    pygame.draw.aaline(screen, (255, 255, 255), (LINE_START[0], LINE_START[1]), (line_end_x, line_end_y))
 
     # Update the display
     pygame.display.update()
