ca2948a135a0077e5e9c3607cbcf4e44e391d13e
o2161405 <91959604+o2161405@users.noreply.github.com>
2024-07-06 17:37:55 +0100

    Recommit because the last one used the wrong file

--- source/main.py
+++ source/main.py
@@ -1,6 +1,7 @@
+from email import message_from_string
 import dolphin_memory_engine as DME
 from time import sleep
-import os
+import os, csv
 
 clear = lambda: os.system('cls' if os.name=='nt' else 'clear')
 
@@ -14,33 +15,45 @@ class Memory:
             self.memory = memory
             self.quaternions_address = memory.Resolve_Address(0x809C18F8, [0x20, 0x0, 0x24, 0x90, 0x4, 0xF0])
             self.positions_address = memory.Resolve_Address(0x809C18F8, [0x20, 0x0, 0x0, 0x8, 0x90, 0x18])
-            self.boosts_address = memory.Resolve_Address(0x809C18F8, [0x20, 0x0, 0x44, 0x10C])
-            self.speed_address = memory.Resolve_Address(0x809C18F8, [0x20, 0x0, 0x0, 0x28, 0x20])
-            self.completion_address = memory.Resolve_Address(0x809BD730, [0xC, 0x0, 0xC])
-            self.drift_address = memory.Resolve_Address(0x809C18F8, [0x20, 0x0, 0x44, 0xFE])
-            self.airtime_address = memory.Resolve_Address(0x809C18F8, [0x20, 0x0, 0x44, 0x21A])
+            self.m_mushroomTimer_address = memory.Resolve_Address(0x809C18F8, [0x20, 0x0, 0x0, 0x28, 0x188])
+            self.m_speed_address = memory.Resolve_Address(0x809C18F8, [0x20, 0x0, 0x0, 0x28, 0x20])
+            self.m_raceCompletion_address = memory.Resolve_Address(0x809BD730, [0xC, 0x0, 0xC])
+            self.m_mtCharge_address = memory.Resolve_Address(0x809C18F8, [0x20, 0x0, 0x44, 0xFE])
+            self.m_hopPosY_address = memory.Resolve_Address(0x809C18F8, [0x20, 0x0, 0x44, 0x22C])
+            self.m_countdownTimer_address = memory.Resolve_Address(0x809BD730, [0x22])
+            self.m_surfaceFlags_address = memory.Resolve_Address(0x809C18F8, [0x20, 0x0, 0x0, 0x18, 0x18, 0x2C])
+            self.mushroom_count_address = memory.Resolve_Address(0x809C3618, [0x14, 0x90])
             
             self.quaternions = {"x": 0.0, "y": 0.0, "z": 0.0, "w": 0.0}
             self.positions = {"x": 0.0, "y": 0.0, "z": 0.0}
-            self.boosts = {"miniturbo": 0.0, "shroom": 0.0, "trick": 0.0}
-            self.speed = 0.0
-            self.completion = 0.0
-            self.drift = 0
-            self.airtime = 0
+            self.m_mushroomTimer = 0
+            self.m_speed = 0.0
+            self.m_raceCompletion = 0.0
+            self.m_mtCharge = 0
+            self.m_hopPosY = 0.0
+            self.m_countdownTimer = 0
+            self.m_surfaceFlags = 0
+            self.isAboveOffroad = False
+            self.isTouchingOffroad = False
+            self.mushroom_count = 0
         
         def Update(self):
             for i, key in enumerate(["x", "y", "z", "w"]):
                 self.quaternions[key] = DME.read_float(self.quaternions_address + i*4)
             for i, key in enumerate(["x", "y", "z"]):
                 self.positions[key] = DME.read_float(self.positions_address + i*4)
-            for i, key in enumerate(["miniturbo", "shroom", "trick"]):
-                self.boosts[key] = DME.read_float(self.boosts_address + i*4)
             
-            self.speed = DME.read_float(self.speed_address)
-            self.completion = DME.read_float(self.completion_address)
-            self.drift = int.from_bytes(DME.read_bytes(self.drift_address, 2), byteorder='big', signed=False)
-            self.airtime = int.from_bytes(DME.read_bytes(self.airtime_address, 2), byteorder='big', signed=False)
-
+            self.m_speed = DME.read_float(self.m_speed_address)
+            self.m_raceCompletion = DME.read_float(self.m_raceCompletion_address)
+            self.m_mushroomTimer = int.from_bytes(DME.read_bytes(self.m_mushroomTimer_address, 2), byteorder='big', signed=False)
+            self.m_mtCharge = int.from_bytes(DME.read_bytes(self.m_mtCharge_address, 2), byteorder='big', signed=False)
+            self.m_hopPosY = DME.read_float(self.m_hopPosY_address)
+            self.m_countdownTimer = int.from_bytes(DME.read_bytes(self.m_countdownTimer_address, 2), byteorder='big', signed=False)
+            self.m_surfaceFlags = int.from_bytes(DME.read_bytes(self.m_surfaceFlags_address, 4), byteorder='big', signed=False)
+            self.isAboveOffroad = self.m_surfaceFlags & (1 << (6 - 1)) != 0
+            self.isTouchingOffroad = self.m_surfaceFlags & (1 << (7 - 1)) != 0
+            self.mushroom_count = int.from_bytes(DME.read_bytes(self.mushroom_count_address, 4), byteorder='big', signed=False)
+            
     def __init__(self):
         self.player = Memory.Player(self)
 
@@ -52,6 +65,8 @@ class Memory:
 
 def main():
 
+    previous_time = 0
+    
     while not DME.is_hooked():
         clear()
         print("Not hooked.")
@@ -68,17 +83,36 @@ def main():
         mem = Memory()
         
         while RaceManager.isInRace():
+            
             mem.Update()
-            clear()
-            print(f"""Quaternions: {mem.player.quaternions}
-Positions: {mem.player.positions}
-Boosts: {mem.player.boosts}
-Speed: {mem.player.speed}
-Race Completion: {mem.player.completion}
-Drift: {mem.player.drift}
-Airtime: {mem.player.airtime}
-""")
-            sleep(0.1)
+            
+            print(mem.player.m_surfaceFlags_address, mem.player.mushroom_count)
+            
+            """
+            print(f"Started Capture to race_data.csv")
+
+            with open("race_data.csv", "w", newline="") as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow(["m_countdownTimer","quaternions_x", "quaternions_y", "quaternions_z", "quaternions_w",
+                                 "positions_x", "positions_y", "positions_z",
+                                 "m_mushroomTimer", "m_speed", "m_raceCompletion", "m_mtCharge", "m_hopPosY"])
+                
+                while mem.player.m_raceCompletion < 4.0:    
+                    
+                    mem.Update()
+                    
+                    # Combine dictionary values and variables into a single list
+                    data_row = [mem.player.m_raceCompletion] + [mem.player.quaternions[key] for key in ["x", "y", "z", "w"]] + \
+                        [mem.player.positions[key] for key in ["x", "y", "z"]] + \
+                        [mem.player.m_mushroomTimer, mem.player.m_speed, mem.player.m_raceCompletion,
+                            mem.player.m_mtCharge, mem.player.m_hopPosY]
+                    
+                    writer.writerow(data_row)
+               
+            print(f"Finished writing data, exiting...")
+
+            exit()
+            """
     
 if __name__ == "__main__":
     main()
